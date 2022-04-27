4513f474d278a65b27c2dafe4d88f066507009e7
Izumi Kawashima <schedul.xor@gmail.com>
2022-04-28 01:36:16 +0900

    Compare fareSum and tokunai_pass_price

--- src/components/TokunaiMeter.js
+++ src/components/TokunaiMeter.js
@@ -1,12 +1,19 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import OpenJprailFares from '../openjprailfares';
 
 class TokunaiMeter extends Component{
     render(){
-        return (<div/>);
+        if(this.props.fareSum < OpenJprailFares.tokunai_pass_price){
+            return (<div>{this.props.fareSum} yen? Buy tickets.</div>);
+        }else{
+            return (<div>{this.props.fareSum} yen? Buy {OpenJprailFares.tokunai_pass_price} yen tokunai pass.</div>);
+        }
     }
 }
 
 export default connect((state)=>{
-    return {};
+    return {
+        fareSum: state.fareSum
+    };
 })(TokunaiMeter);
